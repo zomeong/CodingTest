5fbc3032d7e9589091dc07f27d55530001ede97f
Kim JiHyun <123639754+zomeong@users.noreply.github.com>
2023-11-03 11:26:38 +0900

    주석 추가

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -6,6 +6,11 @@
 #define FIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
 
+// 1. brown + yellow = 가로 * 세로
+// 2. brown / 2 + 2 = 가로 + 세로
+// 3. 약수를 모두 찾음
+// 4. 완전탐색으로 더하여 값 찾기
+	
 vector<int> solution(int brown, int yellow) {
 	vector<int> answer;
 	vector<int> prime;
@@ -14,6 +19,7 @@ vector<int> solution(int brown, int yellow) {
 	int xy = brown + yellow;
 	int x_y = brown / 2 + 2;
 
+	// 약수를 찾아 pirme 벡터에 저장
 	for (int i = 1; i <= sqrt(xy); i++) {
 		if (xy % i == 0) {
 			prime.push_back(i);
@@ -22,6 +28,7 @@ vector<int> solution(int brown, int yellow) {
 	}
 	sort(prime.begin(), prime.end());
 
+	// 더해서 x_y가 되는 두 수를 찾아 저장 
 	for (int i = 0; i < prime.size(); i++) {
 		if (prime[i] + prime[prime.size() - i - 1] == x_y) {
 			if (prime[i] >= prime[prime.size() - i]) {
@@ -33,6 +40,7 @@ vector<int> solution(int brown, int yellow) {
 				y = prime[i];
 			}
 		}
+		break;
 	}
 
 	answer.push_back(x);
