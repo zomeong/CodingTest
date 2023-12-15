e5222a020e2246570650170f175c087b31503152
Kim JiHyun <123639754+zomeong@users.noreply.github.com>
2023-12-15 09:43:44 +0900

    Update n＾2 배열 자르기.cpp

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -1,4 +1,3 @@
-#include <string>
 #include <vector>
 
 using namespace std;
@@ -9,22 +8,14 @@ vector<int> solution(int n, long long left, long long right) {
     int x = left / n + 1;
     int y = left % n + 1;
 
-    for (int i = y; i <= n; i++) {
-        if (i <= x) answer.push_back(x);
-        else answer.push_back(i);
-
-        if (answer.size() == right - left + 1) return answer;
-    }
-
     while(1) {
-        x++;
-        for (int i = 1; i <= n; i++) {
+        for (int i = y; i <= n; i++) {
             if (i <= x) answer.push_back(x);
             else answer.push_back(i);
 
             if (answer.size() == right - left + 1) return answer;
         }
+        x++;
+        y = 1;
     }
-
-    return answer;
 }
