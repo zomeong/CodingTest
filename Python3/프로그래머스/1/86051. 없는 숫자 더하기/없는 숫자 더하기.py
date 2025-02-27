561984e1c3ea046fee6b76c8a04af12b3544e92c
Kim JiHyun <123639754+zomeong@users.noreply.github.com>
2025-02-27 19:52:04 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -0,0 +1,11 @@
+def solution(numbers):
+    answer = 0
+    idx = 0
+    numbers.sort()
+    
+    for i in range(10):
+        if i not in numbers:
+            answer += i
+            
+    return answer
+            
