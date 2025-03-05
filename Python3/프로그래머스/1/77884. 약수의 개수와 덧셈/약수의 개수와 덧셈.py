24d79ce35e8bbeb632c59fbd3aaafb1cb2fdebfe
Kim JiHyun <123639754+zomeong@users.noreply.github.com>
2025-03-05 20:56:01 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.41 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ Python3/프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,10 +1,5 @@
 def solution(left, right):
     answer = 0
-    
     for i in range(left, right + 1):
-        if i ** 0.5 == (int)(i ** 0.5):
-            answer -= i
-        else:
-            answer += i
-    
+        answer += -i if i ** 0.5 == (int)(i ** 0.5) else i
     return answer
