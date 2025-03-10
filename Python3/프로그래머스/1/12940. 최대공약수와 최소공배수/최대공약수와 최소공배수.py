9accf742a4ec2358db1e89edabbc3af238d7f236
Kim JiHyun <123639754+zomeong@users.noreply.github.com>
2025-03-10 16:56:09 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.26 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ Python3/프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,12 +1,14 @@
 def solution(n, m):
 
+    gcd = 1
     if n > m:
         n, m = m, n
         
-    for i in range(1, n + 1):
+    for i in range(n, 0, -1):
         if n % i == 0 and m % i == 0:
             gcd = i
+            break
             
-    lcd = n * m // gcd
+    lcd = n * m / gcd
     
     return [gcd, lcd]
