0686481ce3a98ad088f8815809e3ae5afeae1fd2
Kim JiHyun <123639754+zomeong@users.noreply.github.com>
2025-03-12 21:10:30 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -0,0 +1,15 @@
+import math
+
+def solution(n):
+    t, answer, cnt = '', 0, 0
+    
+    while n > 0:
+        t += str(n % 3)
+        n //= 3
+        
+    for i in reversed(t):
+        answer += int(i) * pow(3, cnt)
+        cnt += 1
+        
+    return answer
+        
