429041cec829146d01c42a6f4a685837440d10c3
Kim JiHyun <123639754+zomeong@users.noreply.github.com>
2025-03-30 16:33:12 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 9.43 MB -BaekjoonHub

--- Python3/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ Python3/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,15 +1,7 @@
 def solution(s):
     dct = {
-        "zero":"0",
-        "one":"1",
-        "two":"2",
-        "three":"3",
-        "four":"4",
-        "five":"5",
-        "six":"6",
-        "seven":"7",
-        "eight":"8",
-        "nine":"9"
+        "zero":"0", "one":"1", "two":"2", "three":"3", "four":"4",
+        "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"
     }
     
     for d in dct:
