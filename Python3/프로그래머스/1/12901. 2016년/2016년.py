7ba05b55cea63e2f6b7d8267c81372d4ce3b935d
Kim JiHyun <123639754+zomeong@users.noreply.github.com>
2025-06-01 19:18:24 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 9.26 MB -BaekjoonHub

--- Python3/프로그래머스/1/12901. 2016년/2016년.py
+++ Python3/프로그래머스/1/12901. 2016년/2016년.py
@@ -1,6 +1,6 @@
 def solution(a, b):
-    day = ["THU", "FRI","SAT","SUN","MON","TUE","WED"]
-    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    day = ["THU","FRI","SAT","SUN","MON","TUE","WED"]
+    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30]
     
     date = b + sum(month[0:a - 1])
     
