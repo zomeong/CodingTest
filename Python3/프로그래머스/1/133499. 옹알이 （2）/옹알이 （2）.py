c1bc4657de7ce4dc0809208e92ae33b938da7aa7
Kim JiHyun <123639754+zomeong@users.noreply.github.com>
2025-08-04 16:47:46 +0900

    [level 1] Title: 옹알이 (2), Time: 0.27 ms, Memory: 9.15 MB -BaekjoonHub

--- Python3/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ Python3/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,12 +1,9 @@
 def solution(babbling):
     answer = 0
     pron = ["aya", "ye", "woo", "ma"]
-    pron2 = ["ayaaya", "yeye", "woowoo", "mama"]
-    
-    for p2 in pron2:
-        babbling = [b.replace(p2, '1') for b in babbling]
     
     for p in pron:
+        babbling = [b.replace(p*2, '1') for b in babbling]
         babbling = [b.replace(p, '2') for b in babbling]
     
     for b in babbling:
