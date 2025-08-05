65172ef204ccfcfd6e5cb44dd6476532d775af80
Kim JiHyun <123639754+zomeong@users.noreply.github.com>
2025-08-06 02:43:16 +0900

    [level 1] Title: 숫자 짝꿍, Time: 298.46 ms, Memory: 26.8 MB -BaekjoonHub

--- Python3/프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ Python3/프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -2,11 +2,16 @@ from collections import Counter
 
 def solution(X, Y):
     answer = ''
+    
+    # 문자열의 문자 요소 카운트
     x_count = Counter(X)
     y_count = Counter(Y)
+    
+    # 교집합 = 공통 정수 카운트 딕셔너리
     union = x_count & y_count
+    
+    # 내림차순 정렬
     reunion = sorted(union.items(), key = lambda x:int(x[0]), reverse=True)
-    print(reunion)
     
     if len(reunion) == 0:
         return "-1"
