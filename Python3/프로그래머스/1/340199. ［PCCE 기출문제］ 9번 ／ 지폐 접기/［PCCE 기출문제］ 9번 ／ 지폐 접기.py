60a1e34e272fcfd2ed2c92a16fc6e44038fbff31
Kim JiHyun <123639754+zomeong@users.noreply.github.com>
2025-08-06 22:02:04 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.02 ms, Memory: 9.1 MB -BaekjoonHub

--- Python3/프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
+++ Python3/프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
@@ -2,9 +2,8 @@ def solution(wallet, bill):
     answer = 0
     
     while min(bill) > min(wallet) or max(bill) > max(wallet):
-        if bill[0] < bill[1]:
-            bill[0], bill[1] = bill[1], bill[0]
-        bill[0] //= 2
+        bill.sort()
+        bill[1] //= 2
         answer += 1
     
     return answer
