953b5dd786fe90827c2cf5cf4acb060f1501f9e4
Kim JiHyun <123639754+zomeong@users.noreply.github.com>
2025-10-15 22:01:24 +0900

    [level 2] Title: 주차 요금 계산, Time: 2.08 ms, Memory: 9.66 MB -BaekjoonHub

--- Python3/프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ Python3/프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -9,21 +9,25 @@ def solution(fees, records):
     
     for r in records:
         time, number, type_ = r.split()
-        minutes = int(time[:2]) * 60 + int(time[3:])
+        minutes = int(time[:2]) * 60 + int(time[3:])    # 분으로 환산
         
         if type_ == "IN":
-            cars[number]["IN"] = minutes
+            # 입차시 시간 기록
+            cars[number]["IN"] = minutes    
         else:
+            # 출차시 주차 시간 누적 후 입사 기록 초기화 
             out_time = minutes
             parking_time = out_time - cars[number]["IN"]
             cars[number]["TIME"] += parking_time
             cars[number]["IN"] = None
                  
     for number in sorted(cars.keys()):
+        # 출차 기록 없는 차량 기본값으로 주차 기록 누적
         end_time = 23 * 60 + 59
         if cars[number]["IN"] is not None:
             cars[number]["TIME"] += end_time - cars[number]["IN"]
             
+        # 주차비 계산
         parking_time = cars[number]["TIME"]
         fee = defalut_fee
 
